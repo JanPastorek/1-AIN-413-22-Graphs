0c1092a9e3a216dcccd5bc6c6ba577d287aaeaac
jp <jpastorek5@gmail.com>
2025-08-25 18:27:25 +0200

    github classroom prep

---
+++ solution_1.py
@@ -0,0 +1,20 @@
+
+import networkx as nx
+from typing import List
+
+# you are forbidden to use any built-in functions for this exercise
+
+
+def havel_hakimi(sequence: List[int]) -> bool:
+    # TODO: implement the Havel-Hakimi algorithm on your own
+    # return true or false if the sequence is graphical or not
+
+    return True
+
+
+def all_graphs_from_degree_sequence(sequence: List[int]) -> List[nx.Graph]:
+    # TODO:
+    # return list of all non-isomorphic graphs constructed from the sequence
+    # on the input, if its graphical.
+    # If the sequence is not graphical, return an empty list.
+    return []
---
+++ test_1.py
@@ -0,0 +1,90 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from typing import List
+from solution_1 import havel_hakimi, all_graphs_from_degree_sequence
+
+
+def test_havel_hakimi():
+    # note that these are the same seq. as we did by hand on exercises
+    list1 = [2, 2, 2, 1, 1]
+    list2 = [2, 2]
+    list3 = [2, 2, 2, 2]
+    list4 = [3, 3, 5, 1, 7, 9, 1]
+    list5 = [3, 3, 3, 3, 3, 3, 3, 3]
+    list6 = [6, 5, 5, 4, 3, 3, 3, 2, 2]
+    list7 = [3, 3, 2, 2, 1, 1]
+    list8 = [7, 6, 4, 3, 3, 2]
+    list9 = [3, 3, 1, 1]
+    list10 = [5, 4, 3, 3, 2, 2, 2, 1, 1, 1]
+
+    # check if the sequence is graphical or not
+    assert havel_hakimi(list1) == nx.is_graphical(list1, method="hh")
+    print("Test 1 passed ", str(list1), " is graphical? ",
+          nx.is_graphical(list1, method="hh"))
+    assert havel_hakimi(list2) == nx.is_graphical(list2, method="hh")
+    print("Test 2 passed ", str(list2), " is graphical? ",
+          nx.is_graphical(list2, method="hh"))
+    assert havel_hakimi(list3) == nx.is_graphical(list3, method="hh")
+    print("Test 3 passed ", str(list3), " is graphical? ",
+          nx.is_graphical(list3, method="hh"))
+    assert havel_hakimi(list4) == nx.is_graphical(list4, method="hh")
+    print("Test 4 passed ", str(list4), " is graphical? ",
+          nx.is_graphical(list4, method="hh"))
+    assert havel_hakimi(list5) == nx.is_graphical(list5, method="hh")
+    print("Test 5 passed ", str(list5), " is graphical? ",
+          nx.is_graphical(list5, method="hh"))
+    assert havel_hakimi(list6) == nx.is_graphical(list6, method="hh")
+    print("Test 6 passed ", str(list6), " is graphical? ",
+          nx.is_graphical(list6, method="hh"))
+    assert havel_hakimi(list7) == nx.is_graphical(list7, method="hh")
+    print("Test 7 passed ", str(list7), " is graphical? ",
+          nx.is_graphical(list7, method="hh"))
+    assert havel_hakimi(list8) == nx.is_graphical(list8, method="hh")
+    print("Test 8 passed ", str(list8), " is graphical? ",
+          nx.is_graphical(list8, method="hh"))
+    assert havel_hakimi(list9) == nx.is_graphical(list9, method="hh")
+    print("Test 9 passed ", str(list9), " is graphical? ",
+          nx.is_graphical(list9, method="hh"))
+    assert havel_hakimi(list10) == nx.is_graphical(list10, method="hh")
+    print("Test 10 passed ", str(list10), " is graphical? ",
+          nx.is_graphical(list10, method="hh"))
+
+
+def display_graphs(graphs: List[nx.Graph]):
+    # display all graphs in the list
+    for i, g in enumerate(graphs):
+        nx.draw(g, with_labels=True)
+    plt.show()
+
+
+def test_havel_hakimi_2():
+    # note that these are the same seq. as we did by hand on exercises
+    list1 = [2, 2, 2, 1, 1]
+    list2 = [3, 3, 3, 3, 3, 3]
+    list3 = [3, 3, 3]
+
+    set1 = all_graphs_from_degree_sequence(list1)
+    set2 = all_graphs_from_degree_sequence(list2)
+    set3 = all_graphs_from_degree_sequence(list3)
+    display_graphs(set1)
+    display_graphs(set2)
+    display_graphs(set3)
+
+    assert len(set1) == 3
+    assert len(set2) == 1
+    assert len(set3) == 0
+
+    set_1_correct = [nx.from_graph6_bytes(b'>>graph6<<DqC')]
+    set_2_correct = [nx.from_graph6_bytes(
+        b'>>graph6<<EuLg'),  nx.from_graph6_bytes(b'>>graph6<<EtXW')]
+
+    solutions_tests = [(set1, set_1_correct), (set2, set_2_correct)]
+
+    for sol_t in solutions_tests:
+        for g in sol_t[0]:
+            is_in = False
+            for g_corr in sol_t[1]:
+                is_in = nx.is_isomorphic(g, g_corr)
+                if is_in:
+                    break
+            assert is_in == True

d212361cbc23ca88380f0ea8aff1ebe9902498a4
jp <jpastorek5@gmail.com>
2025-10-10 09:48:12 +0200

    asymmetric depth

--- home_project_2_solution.py
+++ home_project_2_solution.py
@@ -1,5 +1,7 @@
 import networkx as nx
 
+# you are allowed to use any function in networkx library including nx.is_isomorphic, nx.vf2pp_all_isomorphisms, etc.
+
 
 def asymmetric_depth(Graph) -> int:
     # TODO: implement algorithm that will return the depth of asymmetry
